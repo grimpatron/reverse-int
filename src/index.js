b921d092ab91df5978ae589cd6318ef24a6920c0
grimpatron <Hellhammer220bpm@gmail.com>
2023-08-04 14:55:03 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let numAbs = Math.abs(n);
+  let str = numAbs + "";
+  let arr = Array.from(str);
+  let arrReverse = arr.reverse();
+  let result = "";
+  for (let i = 0; i < arrReverse.length; i++) {
+    result += arrReverse[i];
+  }
+  return result * 1;
 }
