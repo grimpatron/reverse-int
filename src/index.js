7a43a9c75b2ca027d75844637dc3da2d7fe2401b
grimpatron <hellhammer220bpm@gmail.com>
2024-08-13 12:07:52 +0300

    refactor: change execution method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 module.exports = function reverse (n) {
-  let numAbs = Math.abs(n);
-  let str = numAbs + "";
-  let arr = Array.from(str);
-  let arrReverse = arr.reverse();
-  let result = "";
-  for (let i = 0; i < arrReverse.length; i++) {
-    result += arrReverse[i];
-  }
-  return result * 1;
+  const absNumber = Math.abs(n);
+  const arr = absNumber.toString();
+  return +arr.split('').reverse().join('');
 }
